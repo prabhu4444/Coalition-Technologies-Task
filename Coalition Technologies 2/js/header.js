9b9060dae257270537920094b9c92cd5e13dd0a1
Prabhu Satyam <61291869+prabhu4444@users.noreply.github.com>
2024-04-20 05:53:32 +0530

    Delete Coalition Technologies 2 directory

--- Coalition Technologies 2/js/header.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function(e){
-        if ($(window).scrollTop() > 92) {
-            $('.header').addClass('white-bg')
-        } else {
-            $('.header').removeClass('white-bg')
-        }
-    });
-});
